982b6b4a0977ac7c197dfa00f4402a2957628dab
RaymanCarney <Raymancarney@gmail.com>
2024-03-22 07:57:38 -0400

    Added car class, service, and start

--- mercurySedan.js
+++ mercurySedan.js
@@ -6,10 +6,22 @@ let v = new VehicleModule.Vehicle("Mercury", "Sedan", "1965", "color", "mileage"
 console.log(v.make)
 
 
+
 //After you write the derived Car class, you should test it out.
 
 //Note: You can code your derived Car class here or make a file named index.js and do it there.
-
+class Car extends VehicleModule {
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage)
+        this.maxPassengers = 5
+        this.passengers = 0
+        this.numberOfWheels = 4
+        this.maximumSpeed = 160
+        this.fuel = 10
+        this.scheduleService = false
+    }
+    
+}
 
 //TO DO: Code the Car subclass here or in index.js file, i.e. class Car extends Vehicle ...
 
